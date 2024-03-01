100158870557417c5f110a67a23104bbdcc7c71d
Robins <robinmgk077@gmail.com>
2024-03-01 18:09:05 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,28 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed=None):
+        if breed and breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.name = name
+            self.breed = breed
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str and 0 < len(name) <= 25):
+            print(f"Setting name to { name }.")
+            self._name = name
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+
+fido = Dog()
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,24 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name='Guido', job='Sales'):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.name = name
+            self.job = job
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name.title()
+
+    def set_name(self, name):
+        if type(name) == str and 0 < len(name) <= 25:
+            print(f"Setting name to {name}.")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
